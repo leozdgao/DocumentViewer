f79babfb4d8f19dc04a0c19adc5ef688756a14a6
LEOxiaoD <gzdleo0816@hotmail.com>
2014-10-18 14:41:09 +0800

    fix bugs
    no longer use cdn, and export docs as a function.

--- docs/docs.js
+++ docs/docs.js
@@ -2,78 +2,68 @@ var fs = require("fs");
 var path = require("path");
 var async = require("async");
 var md = require("markdown");
-var rootPath = "/docs/";
 
-/*
-{
-	directories: {
-		directories: {
-			files: []
-		},
-		files: []
-	}, files: []
+module.exports = function(rootPath) {
+	var availableExt = [
+		".markdown",
+		".mdown",
+		".mkdn",
+		".md",
+		".mkd",
+		".mdwn",
+		".mdtxt",
+		".mdtext",
+		".text",
+		".txt"
+	];
 
-}
+	return {
+		getStructure: function(root, callback) {
+			var structure = {
+				directories: [],
+				files: []
+			};
+			var root = path.join(rootPath, root);
 
-function read (root, structure) {
-		var files = fs.readdirSync(root);
-		files.forEach(function (file) {
-			var dir = path.join(root, file);
-			var stat = fs.statSync(dir);
-			if(stat.isFile()) structure.files.push(file);
-			else if(stat.isDirectory()) {
-				structure.directories[file] = {
-					directories: {},
-					files: []
-				};
+			async.waterfall([
+					function(cb) {
+						fs.readdir(root, cb);
+					},
+					function(files, cb) {
+						async.eachSeries(files, function(file, callback) {
+							var dir = path.join(root, file);
+							var stat = fs.statSync(dir);
+							if(stat.isFile()) {
+								if(availableExt.indexOf(path.extname(file)) > -1) 
+									structure.files.push(file);
 
-				read(dir, structure.directories[file]);
-			}
-		})
-	}
-*/
-exports.getStructure = function(root, callback) {
-	var structure = {
-		directories: [],
-		files: []
-	};
-	var root = path.join(rootPath, root);
+								callback();
+							}
+							else if(stat.isDirectory()) {
+								structure.directories.push(file);
 
-	async.waterfall([
-			function(cb) {
-				fs.readdir(root, cb);
-			},
-			function(files, cb) {
-				async.eachSeries(files, function(file, callback) {
-					var dir = path.join(root, file);
-					var stat = fs.statSync(dir);
-					if(stat.isFile()) {
-						structure.files.push(file); callback();
+								callback();	
+							}
+							else callback();
+						}, function(err) {
+							cb(null, structure);
+						});
 					}
-					else if(stat.isDirectory()) {
-						structure.directories.push(file); callback();	
-					} 
-				}, function(err) {
-					cb(null, structure);
+				], function(err, result) {
+					callback(err, result);
 				});
-			}
-		], function(err, result) {
-			callback(err, result);
-		});
-}
-
-exports.isFile = function(root) {
-	var root = path.join(rootPath, root);
-	var stat = fs.statSync(root);
-	return stat.isFile();
-}
-
-exports.toHtml = function(root, cb) {
-	var root = path.join(rootPath, root);
-	fs.readFile(root, { encoding: "utf-8" }, function(err, data) {
-		if(err) cb(err);
-		else {
-			cb(null, md.parse(data));
+		},
+		isFile: function(root) {
+			var root = path.join(rootPath, root);
+			var stat = fs.statSync(root);
+			return stat.isFile();
+		},
+		toHtml: function(root, cb) {
+			var root = path.join(rootPath, root);
+			fs.readFile(root, { encoding: "utf-8" }, function(err, data) {
+				if(err) cb(err);
+				else cb(null, md.parse(data));
+			});
 		}
-	});
+	}
 }
--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,38 @@
-var docs = require("./../docs/docs");
+var docs = require("./../docs/docs")("D:\\docs");
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/*', function(req, res, next) {
 	var path = req.params[0];
+	if(path.charAt(path.length -1) == "/")
+		path = path.substring(0, s.length - 1);
+
+	var vm = {
+		title: 'Document Viewer',
+		path: path,
+		isroot: path == '',
+		join: require("path").join
+	}
+	var li = path.lastIndexOf('/');
+	if(li > -1) {
+		vm.lastpath = path.substring(0, li);
+	} else vm.lastpath = "";
 
 	if(docs.isFile(path)) {
 		docs.toHtml(path, function(err, html) {
 			if(err) next();
 			else {
-				res.render('index', { title: 'Document Viewer', path: path, content: html })
+				vm.content = html;
+				res.render('index', vm)
 			}
 		});
 	} else {
 		docs.getStructure(path, function(err, structure) {
 			if(err) next();
 			else {
-				res.render('index', { title: 'Document Viewer', path: path, structure: structure });
+				vm.structure = structure;
+				res.render('index', vm);
 			}
 		});
 	}
