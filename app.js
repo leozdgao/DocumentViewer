42f29c0e582dc68588212728f8fa133b8e4da8de
leozdgao <gzdleo0816@gmail.com>
2015-01-21 16:01:09 +0800

    remove slides

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var docPath = config.docpath || _join(__dirname, 'example'),
 
 //load module
 var express = require('express'),
-    Then = require("thenjs");
     docs = require('./docs')(docPath);
 
 var app = express(),
@@ -32,62 +31,6 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.static(_join(__dirname, 'public')));
-
-app.use('/slides/*', function(req, res) {
-
-    var docs = require('./docs')(_join(__dirname,config.slidespath));
-    var excludeFolder = ["css", "js", "lib", "plugin"];
-    var slidepath = req.params[0].replace(/\/$/, '');
-
-    var breadcrumbs = slidepath.split("/") || [];
-    var last = "/"
-    breadcrumbs = breadcrumbs.map(function(item, index) {
-
-        if(index == breadcrumbs.length - 1) {
-            return {
-                text: item    
-            }
-        }
-        else {
-            last = _join(last, item);
-            return {
-                text: item,
-                link: last
-            }    
-        }
-    });
-    
-    var vm = {
-        title: 'Document Viewer',
-        breadcrumbs: breadcrumbs,
-        path: slidepath,
-        isNotRoot: !!slidepath,
-        type: "slides"
-    }
-
-    docs.getStructure(slidepath, excludeFolder, function(err, structure, readme) {
-        if(err) {
-            res.render('error', { err: err });
-        }
-        else {
-            vm.content = readme;
-            vm.structure = structure;
-            res.render('slides', vm);
-        }
-    });
-    // require("fs").readdir(slidepath, function(err, files) {
-
-    //     
-    //     files = files.filter(function(item) {
-    //         return !excludeFolder.some(function(exclude) {
-    //             return exclude == item;
-    //         });
-    //     });
-
-    //     console.log(files);
-    //     res.end();
-    // });
-});
 
 //add images path if exist
 if(!/^\s*$/.test(imagesPath)) app.use(express.static(imagesPath));
